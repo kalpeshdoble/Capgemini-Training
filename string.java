987af5cb7c04b734e3e360f04cc5841b9285d094
Kalpesh Doble <kalpeshdoble31@gmail.com>
2025-01-07 12:24:58 +0530

    hello

---
+++ string.java
@@ -0,0 +1,9 @@
+public class variable{
+            
+               
+                public static void main(String[] args) {
+                   System.out.println("aamaksk");
+                   System.out.println("sakxa");
+                   System.out.println("954578");
+                }
+            }
